3daa93aabcc548b4e0536037fffffa5625f108ce
Anyona-00 <ombasaanyona@gmail.com>
2024-08-14 11:11:03 +0300

    fixed the image display issue

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import spaceImage from "./space.jpg";
+import jeffImage from "./jeff.jpeg";
+import moonImage from "./moon.jpg";
+import normalImage from "./normal.jpg";
 
 // Setup
 
@@ -69,12 +73,12 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+const spaceTexture = new THREE.TextureLoader().load(spaceImage);
 scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load("jeff.jpeg");
+const jeffTexture = new THREE.TextureLoader().load(jeffImage);
 
 const jeff = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
@@ -85,8 +89,8 @@ scene.add(jeff);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load("moon.jpg");
-const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+const moonTexture = new THREE.TextureLoader().load(moonImage);
+const normalTexture = new THREE.TextureLoader().load(normalImage);
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
